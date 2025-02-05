ff98a7f4c4c70387893ea0a0b0d65c8e0b9ebca4
rquesada <rquesadaqq@outlook.com>
2025-02-04 23:16:52 -0600

    Se agrega day 5 del bootcamp

--- day_4_rock_paper_scissor/main.py
+++ day_4_rock_paper_scissor/main.py
@@ -6,6 +6,7 @@ def play():
     computer = random.choice(['r', 'p', 's'])
 
     if user == computer:
+
         print("Computer chose", computer)
         return 'It\'s a tie'
 
@@ -21,6 +22,34 @@ def play():
 def is_win(user, computer):
     
     if (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
+        if user == 'r':
+            print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+      
+""")
+        elif user == 's':
+            print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+        else:
+            print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")            
         return True
     return False
 
