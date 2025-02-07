9bd8d8db1b1ebfbbb03ed9a8ed0a2a9871aab952
rquesada <rquesadaqq@outlook.com>
2025-02-07 01:39:12 -0600

    avance en el email_test

---
+++ Python-scripts/email_test/emails.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+import csv
+import sys
+
+def populate_dictionary(filename):
+  """Populate a dictionary with email addresses and full names from a CSV file"""
+  email_dict = {}
+  with open(filename, newline='') as csvfile:
+    lines = csv.reader(csvfile)
+    for row in lines:
+      email_dict[row[0]] = row[1]
+  return email_dict
+
+def find_email(argv):
+  """Return an email address based on the username given"""
+  # Create the username based on the command line input
+  try:
+    fullname = str(argv[1] + " " + argv[2])
+    # Preprocess the data
+    email_dict = populate_dictionary('C:/Users/jinch/mi-portafolio-python/python-scripts/email_test/user_emails.csv')
+    # Find and print the email
+    print(email_dict)
+    if email_dict.get(fullname.lower()):
+      return email_dict.get(fullname.lower())
+    else:
+      return "No email address found"
+  except IndexError:
+    return "Missing parameters"
+
+sys.argv.append("Blossom")
+sys.argv.append("Gill")
+print(find_email(sys.argv))
