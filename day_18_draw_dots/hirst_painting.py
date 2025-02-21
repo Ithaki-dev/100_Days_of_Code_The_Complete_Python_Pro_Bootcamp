9640b69201009e221f7215ea4f61349fc70b31d9
rquesada <rquesadaqq@outlook.com>
2025-02-21 00:55:50 -0600

    add hirst_painting.py and install cologram module

---
+++ day_18_draw_dots/hirst_painting.py
@@ -0,0 +1,5 @@
+import colorgram
+
+colors = colorgram.extract('C:\\Users\\jinch\\mi-portafolio-python\\day_18_draw_dots',6)
+
+print(colors)
--- day_18_draw_dots/main.py
+++ day_18_draw_dots/main.py
@@ -1,21 +1,49 @@
 # This is a program for creating dots paints
 
 import random
+import turtle as t
 from turtle import Turtle, Screen
 
 
 bobby = Turtle()
-bobby.shape("circle")
-color_list = ["black", "red", "green", "yellow", "blue", "brown", "orange", "purple"]
+bobby.shape("arrow")
+bobby.pensize(3)
+bobby.speed("fastest")
+t.colormode(255)
 
+
+color_list = ["black", "red", "green", "yellow", "blue", "brown", "orange", "purple"]
+direction = [0,90,180,270]
 def drawShape(num):
     bobby.color(random.choice(color_list))
+    angle = 360 / num
     for _ in range(num):
         bobby.forward(100)
-        bobby.right(360/num)
+        bobby.right(angle)
 
-for i in range(3,11):
-    drawShape(i)
+def randomColor():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def circle():
+    bobby.color(randomColor())
+    bobby.circle(200)
+
+
+
+def randomWalk(num):
+    for _ in range(num):
+        bobby.color(randomColor())
+        bobby.forward(30)
+        bobby.setheading(random.choice(direction))
+        
+
+#for i in range(3,11):
+#   drawShape(i)
+#print(randomColor())
+#randomWalk(100)
+for i in range(100):
+    circle()
+    bobby.left(5)
     
 
 
