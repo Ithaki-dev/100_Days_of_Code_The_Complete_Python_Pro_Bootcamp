b345177400976239d87b972f2281a9a681d553e3
rquesada <rquesadaqq@outlook.com>
2025-02-22 20:06:09 -0600

    Finish day 19 turtle race

--- day_19_turtle_coordinate/main.py
+++ day_19_turtle_coordinate/main.py
@@ -3,11 +3,30 @@ from turtle import Turtle, Screen
 bobby = Turtle()
 screen = Screen()
 
-def move_forward():
-    bobby.forward(100)
+key = " "
 
-bobby.teleport(-215,-215)
+def move_forward(): 
+    bobby.forward(50)
+
+def move_backward():
+    bobby.backward(50)
+
+def turn_left():
+    bobby.left(10)
+
+def turn_right():
+    bobby.right(10)
+
+def clear():
+    bobby.clear()
+    bobby.penup()
+    bobby.home()
+
 screen.listen()
+screen.onkey(move_backward,"s")
 screen.onkey(move_forward,"w")
+screen.onkey(turn_left,"a")
+screen.onkey(turn_right,"d")
+screen.onkey(clear,"c")
 
 screen.exitonclick()
---
+++ day_19_turtle_coordinate/turtle_race.py
@@ -0,0 +1,40 @@
+import random
+from turtle import Turtle, Screen
+
+#bobby = Turtle(shape="turtle")
+screen = Screen()
+screen.setup(width=500, height= 400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race?")
+color_list = ["red", "green", "blue", "yellow", "brown","black"]
+is_race_on = False
+all_turtles = []
+
+for color in color_list:
+    b = Turtle()
+    b.color(color)
+    b.shape("turtle")
+    b.penup()
+    b.goto(-200, 100 - (color_list.index(color) * 40))
+    all_turtles.append(b)
+
+if user_bet:
+    is_race_on = True
+
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        rand_distance = random.randint(0,15)
+        turtle.forward(rand_distance)
+        if turtle.xcor() > 200:
+            is_race_on = False
+            winning_color = turtle.color()
+            print(winning_color)
+            if winning_color[0] == user_bet:
+                print(f"You've won! The {winning_color[0]} turtle won the race.")
+                break
+            else:
+                print(f"You've lost! The {winning_color[0]} turtle won the race.")
+                break
+            
+screen.exitonclick()
