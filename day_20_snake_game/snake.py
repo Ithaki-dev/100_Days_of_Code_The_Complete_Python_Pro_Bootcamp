50dcaf9c6ce262553ed4a20e66ac4cd911ff5560
rquesada <rquesadaqq@outlook.com>
2025-02-22 23:08:02 -0600

    Add day 20 the famous snake game

---
+++ day_20_snake_game/snake.py
@@ -0,0 +1,36 @@
+from turtle import Screen, Turtle
+
+class Snake():
+    def __init__(self):
+        self.segments = []
+        for i in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(x=-20*i, y=0)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for i in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[i-1].xcor()
+            new_y = self.segments[i-1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        self.segments[0].forward(20)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+    
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+    
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
+    
+    
