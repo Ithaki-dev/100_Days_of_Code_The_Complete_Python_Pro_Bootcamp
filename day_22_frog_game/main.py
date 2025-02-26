427ffa92df33957aaf51f8d36051b8b9b63bf810
rquesada <rquesadaqq@outlook.com>
2025-02-25 21:02:42 -0600

    Add day 22 the frog path game

---
+++ day_22_frog_game/main.py
@@ -0,0 +1,17 @@
+#this is the clasic foog path game
+
+import time
+from turtle import Screen, Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
