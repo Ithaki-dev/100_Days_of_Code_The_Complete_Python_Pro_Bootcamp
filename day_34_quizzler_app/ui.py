53113d643c61c5fcdbc6c0f7b4f8511fbc0dea94
rquesada <rquesadaqq@outlook.com>
2025-03-18 00:40:53 -0600

    Update day 34 update quizzler app adding UI interface

--- day_34_quizzler_app/ui.py
+++ day_34_quizzler_app/ui.py
@@ -1,2 +1,11 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz App")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+
+        self.window.mainloop()
