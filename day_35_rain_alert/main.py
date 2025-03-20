891efb1647c95aba30fba69ab30ed1075bd05c98
rquesada <rquesadaqq@outlook.com>
2025-03-19 23:42:50 -0600

    Update day 35 Rain notify with sending SMS with Twilio API

--- day_35_rain_alert/main.py
+++ day_35_rain_alert/main.py
@@ -1,12 +1,22 @@
 import json
 import requests
+import os
+from twilio.rest import Client
 
+# Weather API
 OWM_Endpoint = "http://api.openweathermap.org/data/2.5/forecast"
-api_key = ""
+api_key = os.environ.get('OWM_API_KEY')
+# Twilio API
+account_sid = 'AC1d9ace12d1f4b751050e6e1ff6fa0059'
+auth_token = os.environ.get('AUTH_TOKEN_TWILIO')
+print(auth_token,api_key)
 
+
+
+# Coordinates of the location where you want to check the weather
 weather_params = {
-    "lat": 39.48,
-    "lon": -0.7532,
+    "lat": 38.71,
+    "lon": -9.13,
     "appid": api_key,
     "units": "metric",
     "cnt": 4  # Number of days to get forecast for
@@ -18,8 +28,18 @@ response.raise_for_status()
 
 # # Check every id of the weather in response and if any of them is less than 700 print umbrella 
 
+def send_SMS():
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+            from_='+13312461663',
+            body='Its going to rain',
+            to='+50660437458'
+        )
+    print(message.sid)
+
 weather_data = response.json()["list"]
 for weather_element in weather_data:
     if weather_element["weather"][0]["id"] < 700:
         print("It's going to rain today!")
+        send_SMS()
         break
