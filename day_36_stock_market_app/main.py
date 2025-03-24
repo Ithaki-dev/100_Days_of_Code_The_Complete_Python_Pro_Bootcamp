c43443ddd7e881c284256329bf260ed7b51be66f
rquesada <rquesadaqq@outlook.com>
2025-03-24 00:37:04 -0600

    update day 36 stock market get news from API

--- day_36_stock_market_app/main.py
+++ day_36_stock_market_app/main.py
@@ -1,4 +1,6 @@
 import datetime
+import json
+import os
 import requests
 
 
@@ -7,9 +9,11 @@ COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+red_triangle = "🔺"
+inverted_red_triangle = "🔻"
 # get yesterday date
-yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-day_before_yesterday = (datetime.date.today() - datetime.timedelta(days=2)).strftime("%Y-%m-%d")
+yesterday = (datetime.date.today() - datetime.timedelta(days=3)).strftime("%Y-%m-%d")
+day_before_yesterday = (datetime.date.today() - datetime.timedelta(days=4)).strftime("%Y-%m-%d")
 
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -32,27 +36,36 @@ response = requests.get(STOCK_ENDPOINT, params=parameters)
 data = response.json()
 news_response  = requests.get(STOCK_ENDPOINT,params=news_parameters)
 news_data = news_response.json()
+print(news_data)
 
-print(data)
 
 yesterday_closing_price = data["Time Series (Daily)"][yesterday]["1. open"]
 day_before_yesterday_price = data["Time Series (Daily)"][day_before_yesterday]["4. close"]
 
 # Calculate the diference between yesterday_closing_price and day_before_yesterday_price in %
-def calculate_diference():
+def calculate_difference():
     percentage_difference = ((float(yesterday_closing_price) - float(day_before_yesterday_price)) / float(day_before_yesterday_price)) * 100
     print(percentage_difference )
-    if percentage_difference > 2 or percentage_difference < -2:
-        print("Get News")
-        get_news()
+    if percentage_difference > 2:
+        get_news(red_triangle, percentage_difference)
+    elif percentage_difference < -2:
+        get_news(inverted_red_triangle, percentage_difference)
     else:
         print("No News")
+        get_news(inverted_red_triangle, percentage_difference)
 
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
 #HINT 1: Think about using the Python Slice Operator
-def get_news():
-    news_articles = news_data["articles"][:3]
+# news_path = os.path.join(os.path.dirname(__file__), "news.json")
+# with open(news_path, "r") as file:
+#     news_data = json.load(file)
+#     file.close()
+
+def get_news(triangle,difference):
+    news_title = news_data['feed'][0]['title']
+    news_summary = news_data['feed'][0]['summary']
+    news_articles = f"{STOCK} {triangle}{difference:.2f}%\nHeadline: {news_title}\nBrief: {news_summary}"
     print(news_articles)
    
     
@@ -74,4 +87,4 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-calculate_diference()
+calculate_difference()
