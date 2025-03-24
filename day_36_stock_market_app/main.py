90c97569df59317bac4b851134d1451fc37a2998
rquesada <rquesadaqq@outlook.com>
2025-03-24 01:17:32 -0600

    update day 36 stock market fix news from API to 150 characteres

--- day_36_stock_market_app/main.py
+++ day_36_stock_market_app/main.py
@@ -2,9 +2,9 @@ import datetime
 import json
 import os
 import requests
+from twilio.rest import Client
 
-
-STOCK = "IBM"
+STOCK = "RDDT"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
@@ -19,6 +19,9 @@ day_before_yesterday = (datetime.date.today() - datetime.timedelta(days=4)).strf
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
 #HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+
+account_sid = "AC1d9ace12d1f4b751050e6e1ff6fa0059"#os.environ.get('ACCOUNT_SID')
+auth_token = "4b71ccadd5cf26ae4ce66699d53b0baf"#os.environ.get('AUTH_TOKEN_TWILIO')
 
 parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -36,8 +39,6 @@ response = requests.get(STOCK_ENDPOINT, params=parameters)
 data = response.json()
 news_response  = requests.get(STOCK_ENDPOINT,params=news_parameters)
 news_data = news_response.json()
-print(news_data)
-
 
 yesterday_closing_price = data["Time Series (Daily)"][yesterday]["1. open"]
 day_before_yesterday_price = data["Time Series (Daily)"][day_before_yesterday]["4. close"]
@@ -45,7 +46,6 @@ day_before_yesterday_price = data["Time Series (Daily)"][day_before_yesterday]["
 # Calculate the diference between yesterday_closing_price and day_before_yesterday_price in %
 def calculate_difference():
     percentage_difference = ((float(yesterday_closing_price) - float(day_before_yesterday_price)) / float(day_before_yesterday_price)) * 100
-    print(percentage_difference )
     if percentage_difference > 2:
         get_news(red_triangle, percentage_difference)
     elif percentage_difference < -2:
@@ -64,18 +64,20 @@ def calculate_difference():
 
 def get_news(triangle,difference):
     news_title = news_data['feed'][0]['title']
-    news_summary = news_data['feed'][0]['summary']
-    news_articles = f"{STOCK} {triangle}{difference:.2f}%\nHeadline: {news_title}\nBrief: {news_summary}"
-    print(news_articles)
-   
-    
+    news_summary = news_data['feed'][0]['url']
+    body_message = str(f'{COMPANY_NAME}: {triangle}{difference:.2f}%\nHeadline: {news_title}\nBrief: {news_summary[:150]}')
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+            from_='+13312461663',
+            body=body_message,
+            to='+50660437458'
+        )
+    print(message.sid,body_message)  
 
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
 #HINT 1: Consider using a List Comprehension.
-
-
 
 #Optional: Format the SMS message like this: 
 """
