45d493f13606c750e31d2279d0976eaf713800e4
rquesada <rquesadaqq@outlook.com>
2025-03-25 18:33:56 -0600

    Make, update and delete entrys in my new graph

--- day_37_pixela_app/main.py
+++ day_37_pixela_app/main.py
@@ -1,10 +1,14 @@
 # This is a app for use pixela API
-
 import requests
+import datetime
+
 USERNAME = "rquesada"
 TOKEN = "asd2dqwed23dsd23sd23"
-
 pixela_endpoint = "https://pixe.la/v1/users"
+
+# Get the day in yyyyMMdd format
+today = datetime.datetime.now()
+today = today.strftime("%Y%m%d")
 
 # This is a dictionary with the parameters that we need to send to the API
 user_params = {
@@ -35,9 +39,24 @@ graph_config_params = {
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/d01"
 
 pixel_data = {
-    "date": "20250325",
-    "quantity": "5.32",
+    "date": "20250320",
+    "quantity": "1.11",
 }
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers={"X-USER-TOKEN": TOKEN})
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers={"X-USER-TOKEN": TOKEN})
+# print(response.text)
+
+# Update a pixel
+update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/d01/{today}"
+update_pixel_data = {
+    "quantity": "5.32",
+    "date": today,
+}
+
+# response = requests.put(url=update_pixel_endpoint, json=update_pixel_data, headers={"X-USER-TOKEN": TOKEN})
+# print(response.text)
+
+# Delete a pixel
+delete_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/d01/20250320"
+response = requests.delete(url=delete_pixel_endpoint, headers={"X-USER-TOKEN": TOKEN})
 print(response.text)
