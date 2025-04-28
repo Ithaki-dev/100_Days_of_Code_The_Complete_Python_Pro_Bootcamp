766ee5e553b49dd26b0a2166cddbea7bdca40674
rquesada <rquesadaqq@outlook.com>
2025-04-27 23:38:11 -0600

    Finish day 38 proyect usin nutritionix API

--- day_38_nutritionix_app/main.py
+++ day_38_nutritionix_app/main.py
@@ -9,8 +9,8 @@ API_KEY = "16dfc465b9e45ab7bf17d96915618a55"
 NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 SHEETY_ENDPOINT = "https://api.sheety.co/48bf65172daafcb3ac2a7f35015c2f7a/myWorkouts/workouts"
 
-SHEETY_USERNAME = os.environ.get("SHEETY_USERNAME")
-SHEETY_PASSWORD = os.environ.get("SHEETY_PASSWORD")
+SHEETY_USERNAME = "rquesada"
+SHEETY_PASSWORD = "sdvaeryfcdvbds"
 SHEETY_AUTH = (SHEETY_USERNAME, SHEETY_PASSWORD)
 
 # Get user input for exercise and duration
@@ -30,15 +30,38 @@ headers = {
 }
 params = {
     "query": exercise,
-    # "duration": duration,
-    "time": current_time,
-    "date": current_date,
-    "timezone": "US/Eastern"
 }
 
 # Make a request to the Nutritionix API
-response = requests.post(NUTRITIONIX_ENDPOINT, headers=headers, json=params)
-response.raise_for_status()  # Raise an error for bad responses
-exercise_data = response.json()
+try:
+    response = requests.post(NUTRITIONIX_ENDPOINT, headers=headers, json=params)
+    response.raise_for_status()  # Raise an error for bad responses
+    data = response.json()
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching data from Nutritionix API: {e}")
+    exit(1)\
+    
+# Make a request to the Sheety API to save the data
+try:
+    for exercise in data["exercises"]:
+        headers = {"Authorization": "Basic cnF1ZXNhZGE6c2R2YWVyeWZjZHZiZHM="}
+        sheet_data = {
+            "workout": {
+                "date": current_date,
+                "time": current_time,
+                "exercise": exercise["name"].title(),
+                "duration": exercise["duration_min"],
+                "calories": exercise["nf_calories"],
+            }
+        }
+
+        # Make a request to the Sheety API
+        response = requests.post(SHEETY_ENDPOINT, auth=SHEETY_AUTH, json=sheet_data, headers=headers)
+        response.raise_for_status()  # Raise an error for bad responses
+        print("Data saved to Sheety successfully.")
+except requests.exceptions.RequestException as e:
+    print(f"Error saving data to Sheety API: {e}")
+    exit(1)
+    
 
 
