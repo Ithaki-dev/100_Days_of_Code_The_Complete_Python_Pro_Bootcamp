d9c0b3772d2286e9e881b67cb049c80428c73075
rquesada <rquesadaqq@outlook.com>
2025-04-30 01:46:39 -0600

    get the access token

--- day_39_flight_deal_finder/flight_search.py
+++ day_39_flight_deal_finder/flight_search.py
@@ -16,26 +16,15 @@ city_code = "SJO"  # Juan Santamaria International Airport
 # currency code for Costa Rica
 currency_code = "USD"  # US Dollar
 
-# set environment variables
-# os.environ['FLIGHT_SEARCH_ENDPOINT'] = 'https://test.api.amadeus.com'
-# os.environ['FLIGHT_SEARCH_API_KEY'] = 'bAA7mgPqX2APeO736903ZYBDKZGYLeiX'  # Replace with your actual Flight Search API Key
-# os.environ['FLIGHT_SEARCH_AUTH'] = 'jvjGP6lMd6v5A4EZ'  # Replace with your actual Flight Search API Key
-# os.environ['FLIGHT_SEARCH_HEADERS'] = 'your_headers'  # Replace with your actual Flight Search API Key
-# os.environ['FLIGHT_SEARCH_PARAMS'] = 'your_params'  # Replace with your actual Flight Search API Key
-
 # Get the environment variables
 FLIGHT_SEARCH_ENDPOINT = os.getenv('FLIGHT_SEARCH_ENDPOINT')
 FLIGHT_SEARCH_API_KEY = os.getenv('FLIGHT_SEARCH_API_KEY')
 FLIGHT_SEARCH_AUTH = os.getenv('FLIGHT_SEARCH_AUTH')
-# FLIGHT_SEARCH_HEADERS = os.getenv('FLIGHT_SEARCH_HEADERS')
-# FLIGHT_SEARCH_PARAMS = os.getenv('FLIGHT_SEARCH_PARAMS')
-
-print(FLIGHT_SEARCH_API_KEY, FLIGHT_SEARCH_AUTH, FLIGHT_SEARCH_ENDPOINT)
 
 # Define header and parameters for the Flight Search API request
 headers = {
 
-    "Content-Type": "application/flight_search",
+    "Content-Type": "application/x-www-form-urlencoded",
 }
 params = {
     "grant_type": "client_credentials",
@@ -43,7 +32,7 @@ params = {
     "client_secret": FLIGHT_SEARCH_AUTH,
 }
 
-access_token = requests.post(f"https://test.api.amadeus.com/v1/security/oauth2/token", headers=headers, json=params)
+access_token = requests.post(f"https://test.api.amadeus.com/v1/security/oauth2/token", headers=headers, data=params)
 access_token.raise_for_status()  # Raise an error for bad responses
 access_token = access_token.json()
 access_token = access_token["access_token"]  # Extract the access token from the response
