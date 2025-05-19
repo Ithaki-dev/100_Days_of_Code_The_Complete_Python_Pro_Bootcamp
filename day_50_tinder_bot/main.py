9f61e4a8a435d17cb0c85a34539ee95ce9079c57
rquesada <rquesadaqq@outlook.com>
2025-05-19 00:43:20 -0600

    Advance in the tinder bot using selenium

--- day_50_tinder_bot/main.py
+++ day_50_tinder_bot/main.py
@@ -2,9 +2,24 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
+
+# Configura el perfil de Firefox para permitir la ubicación automáticamente
+profile = FirefoxProfile()
+profile.set_preference("geo.prompt.testing", True)
+profile.set_preference("geo.prompt.testing.allow", True)
+profile.set_preference("geo.enabled", True)
+
+# Usa Options para asignar el perfil
+options = Options()
+options.profile = profile
+
+# Crea una nueva instancia del driver de Firefox con el perfil configurado
+# driver = webdriver.Firefox(firefox_profile=profile)
+driver = webdriver.Firefox(options=options)
+
 # Navigate to the Tinder website
 driver.get("https://tinder.com")
 # Wait for the page to load
@@ -30,12 +45,13 @@ sleep(3)
 driver.switch_to.window(driver.window_handles[1])
 # Find the email and password fields and enter your credentials
 email_field = driver.find_element(By.ID, 'email')
-email_field.send_keys('ithakidev@gmail.com')
+email_field.send_keys('your_email_here')
 password_field = driver.find_element(By.ID, 'pass')
-password_field.send_keys('caraculo123#')
+password_field.send_keys('your_password_here')  # Replace with your actual password
 # Find the login button and click it
 login_button = driver.find_element(By.ID, 'loginbutton')
 login_button.click()
+sleep(3)
 # Click the "Continue" button if it appears
 try:
     continue_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="Continuar como Robert"]')
@@ -52,12 +68,36 @@ try:
     allow_button.click()
 except:
     print("Allow button not found")
-# Allow location access
+sleep(3)
+# Click in i'll miss out
 try:
-    location_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="Allow"]')
-    location_button.click()
+    miss_out_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="I’ll miss out"]')
+    miss_out_button.click()
 except:
-    print("Location button not found")
+    print("Miss out button not found")
 
 # Wait for the Tinder page to load
 sleep(5)
+# acept cookies
+try:
+    cookies_button = driver.find_element(
+        By.CSS_SELECTOR,
+        '[style="--tui-button-background: transparent; --tui-button-border: var(--color--border-button-secondary, inherit); --tui-button-foreground: var(--color--foreground-button-secondary, inherit); --tui-button-overlay: var(--color--interactive-button-secondary, inherit); --tui-button-focus: var(--color--border-focus-default, inherit);"]'
+    )
+    cookies_button.click()
+except:
+    print("Cookies button not found")
+# Wait for the Tinder page to load
+print("Waiting for Tinder to load...")
+sleep(120)
+# Start swiping
+while True:
+    try:
+        # Find the "Like" button and click it
+        like_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="Nope"]')
+        like_button.click()
+        # Wait for a few seconds before swiping again
+        sleep(2)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        break
