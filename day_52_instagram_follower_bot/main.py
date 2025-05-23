3d216f94afc2c1394e299889cb31a74d7cd6fffd
rquesada <rquesadaqq@outlook.com>
2025-05-23 02:47:22 -0600

    Finish day 52 instagram follower bot erase passwords

--- day_52_instagram_follower_bot/main.py
+++ day_52_instagram_follower_bot/main.py
@@ -2,8 +2,8 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-USER_NAME = "ithakidev"
-PASSWORD = "C0n547udr7"
+USER_NAME = "username"
+PASSWORD = "password"
 SIMILAR_ACCOUNT = "chefsteps"
 
 
@@ -11,7 +11,6 @@ class InstagramFollowerBot:
     def __init__(self, similar_account):
         self.similar_account = similar_account
         self.driver = webdriver.Firefox()
-
 
     def login(self):
         username_input = self.driver.find_element(By.NAME, "username")
@@ -24,13 +23,6 @@ class InstagramFollowerBot:
         login_button.click()
         sleep(5)
 
-    def scroll_followers_popup(self, scrolls=5):
-        # Locate the scrollable followers list inside the dialog
-        dialog = self.driver.find_element(By.XPATH, "//div[@role='dialog']//ul")
-        for _ in range(scrolls):
-            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", dialog)
-            sleep(2)
-
     def find_followers(self):
         # Go to the similar account's followers page
         self.driver.get(f"https://www.instagram.com/{self.similar_account}/")
@@ -40,25 +32,32 @@ class InstagramFollowerBot:
         followers_link = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[3]/ul/li[2]/div/a/span")
         followers_link.click()
         sleep(5)
-        # Scroll in the popup to load more followers
-        self.scroll_followers_popup(scrolls=5)
+        # Switch to Pop-up follower frame
+        scrollable_popup = self.driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]")
+        for i in range(10):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_popup)
+            sleep(2)
 
-    
-    # def follow_users(self):
-    #     #  Click on the first 10 followers
-    #     followers = self.driver.find_elements(By.XPATH, "//div[@role='dialog']//li")[:10]
-    #     for follower in followers:
-    #         follow_button = follower.find_element(By.XPATH, ".//button[contains(text(), 'Follow')]")
-    #         if follow_button:
-    #             follow_button.click()
-    #             sleep(2)
 
-  
-    
+    def follow_users(self):
+        # Find all the follow buttons in the followers pop-up
+        follow_buttons = self.driver.find_elements(By.XPATH, "//div[@role='dialog']//button[.//div[text()='Follow']]")
+        if not follow_buttons:
+            print("No follow buttons found.")
+            return
+        for button in follow_buttons:
+            try:
+                button.click()
+                sleep(1)
+            except Exception as e:
+                print(f"Error clicking follow button: {e}")
+                continue
+
+
 if __name__ == "__main__":
     bot = InstagramFollowerBot(SIMILAR_ACCOUNT)
     bot.driver.get("https://www.instagram.com/accounts/login/")
     sleep(5)
     bot.login()
     bot.find_followers()
-    # bot.follow_users()
+    bot.follow_users()
