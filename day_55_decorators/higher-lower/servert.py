2988c687ec8463c99a5cfb6b5fec17d6fdc660a7
rquesada <rquesadaqq@outlook.com>
2025-05-27 00:11:33 -0600

    Finish day 55 higher lower practice

--- day_55_decorators/higher-lower/servert.py
+++ day_55_decorators/higher-lower/servert.py
@@ -3,23 +3,29 @@ from flask import Flask
 from style_text import Stalying
 
 app = Flask(__name__)
+global number
+number = random.randint(0, 9)
 
-# Generate a random number between 0 and 9
-def generate_random_number():
-    """Generate a random number between 0 and 9."""
-    return random.randint(0, 9)
-
-
+def generate_random_number(func):
+    def wrapper(*args, **kwargs):
+        global number
+        number = random.randint(0, 9)
+        print(f"Random number generated: {number}")
+        return func(*args, **kwargs)
+    return wrapper
 
 @app.route('/')
 @Stalying.bold
 #@Stalying.italic
 @Stalying.underline
 @Stalying.center
+@generate_random_number
 def home():
     return "<h1>Welcome to the Higher-Lower Game!</h1>" \
     "<p>Guess a number between 0 and 9.</p>" \
     "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' alt='Number Guessing Game'>"
+
+
 
 @app.route('/higher')
 def higher():
@@ -33,7 +39,7 @@ def lower():
            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' alt='Lower'>"
 
 @app.route('/guess/<int:guess>')
-def main(guess, number=generate_random_number()):
+def main(guess):
     print(f"User guessed: {guess}")
     if guess < number:
         return higher()
@@ -43,6 +49,6 @@ def main(guess, number=generate_random_number()):
         return "<h1>Congratulations! You guessed the number!</h1>" \
                "<p>Play again!</p>" \
                "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' alt='Congratulations'>"
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
