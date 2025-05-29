842af0a104be04a7c02fe26aa285a79ba4a9b08b
rquesada <rquesadaqq@outlook.com>
2025-05-29 02:50:14 -0600

    Add files for the project

--- day_57_jinja_templanting/practice/main.py
+++ day_57_jinja_templanting/practice/main.py
@@ -20,15 +20,23 @@ def get_age(user_name):
     age = response.json().get('age')
     return age
 
-@app.route('/<string:user_name>')
-def home(user_name):
-    user_age = get_age(user_name)
-    user_gender = get_gender(user_name)
+# @app.route('/<string:user_name>')
+# def home(user_name):
+#     user_age = get_age(user_name)
+#     user_gender = get_gender(user_name)
 
-    return render_template('index.html', name=user_name, age=user_age, gender=user_gender)
+#     return render_template('index.html', name=user_name, age=user_age, gender=user_gender)
 
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/3083c920e6d9c3d71c94'
+    response = requests.get(blog_url)
+    blog_posts = response.json()
+    return render_template('blog.html', posts=blog_posts)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
