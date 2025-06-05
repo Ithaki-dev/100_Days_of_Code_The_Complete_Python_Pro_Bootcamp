fcace5b0ab9171a8c026060fdc2352c55f7bccd1
rquesada <rquesadaqq@outlook.com>
2025-06-05 02:15:07 -0600

    Finish in the 61 day form secret

--- day_61_flask_secrets/main.py
+++ day_61_flask_secrets/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import EmailField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-
+from flask_bootstrap import Bootstrap5
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -19,9 +19,10 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your actual secret key
+bootstrap = Bootstrap5(app)
 
 class LoginForm(FlaskForm):
-    username = StringField(label='Username', validators=[DataRequired()])
+    username = EmailField(label='Username', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label='Submit')
 
@@ -37,8 +38,18 @@ def login():
         password = form.password.data
         # Here you would typically check the username and password against a database
         print(f"Username: {username}, Password: {password}")
-        return redirect(url_for('success'))
+        if username == "admin@email.com" and password == "12345678":
+            print("Login successful.")
+            return redirect(url_for('success'))
+        else:
+            print("Login failed. Redirecting to denied page.")
+            return redirect(url_for('denied'))
+        
     return render_template('login.html', form=form)
+
+@app.route("/denied")
+def denied():
+    return render_template('denied.html')
 
 @app.route("/success")
 def success():
