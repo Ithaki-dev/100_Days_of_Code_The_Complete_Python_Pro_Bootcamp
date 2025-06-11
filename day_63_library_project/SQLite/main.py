ca415260022867b006a33d23c598c2d4e4a769a1
rquesada <rquesadaqq@outlook.com>
2025-06-11 01:40:20 -0600

    Finish day 63 library book mark project SQL

--- day_63_library_project/SQLite/main.py
+++ day_63_library_project/SQLite/main.py
@@ -1,18 +1,45 @@
-import sqlite3
+# import sqlite3
 import os
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-# Ensure the database file is created in the current directory
-db = sqlite3.connect(os.path.join(current_directory, "books-collection.db"))
-cursor = db.cursor()
+# # Ensure the database file is created in the current directory
+# db = sqlite3.connect(os.path.join(current_directory, "books-collection.db"))
+# cursor = db.cursor()
 
-def create_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-    db.commit()
+# def create_table():
+#     cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+#     db.commit()
 
-if __name__ == "__main__":
-    # create_table()
-    # print("Table created successfully.")
-    cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-    db.commit()
-    db.close()
+# if __name__ == "__main__":
+#     # create_table()
+#     # print("Table created successfully.")
+#     cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+#     db.commit()
+#     db.close()
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+app = Flask(__name__)
+# Configure the SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(current_directory, "new-books-collection.db")
+# Disable track modifications to save memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Initialize the SQLAlchemy object
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String(250), nullable=False)
+    author = mapped_column(String(250), nullable=False)
+    rating = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    # Create the database and tables
+    #db.create_all()
+    db.session.add(Book(title="Harry Potter", author="J. K. Rowling", rating=9.3))
+    db.session.commit()
+
--- day_63_library_project/main.py
+++ day_63_library_project/main.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+import os
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
 '''
 Red underlines? Install the required packages first: 
@@ -14,6 +19,19 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
+# Configure the SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(current_directory, "SQLite/new-books-collection.db")
+# Disable track modifications to save memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Initialize the SQLAlchemy object
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String(250), nullable=False)
+    author = mapped_column(String(250), nullable=False)
+    rating = mapped_column(Float, nullable=False)
 
 all_books = []
 
@@ -31,6 +49,11 @@ def add():
         book_rating = request.form["rating"]
         all_books.append({"title": book_title, "author": book_author , "rating": book_rating})
         print(all_books)
+        with app.app_context():
+            new_book = Book(title=book_title, author=book_author, rating=float(book_rating))
+            db.session.add(new_book)
+            db.session.commit()
+        # Redirect to the home page after adding the book
         return redirect(url_for("home"))
     return render_template("add.html")
 
