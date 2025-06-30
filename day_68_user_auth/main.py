9f215cd0614062e1aedd523c48d767bc693d8c57
rquesada <rquesadaqq@outlook.com>
2025-06-29 18:53:36 -0600

    Add day 68 user auth learning using flask

---
+++ day_68_user_auth/main.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# CREATE DATABASE
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{current_path}/instance/users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CREATE TABLE IN DB
+
+
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        hashed_password = generate_password_hash(password)
+        new_user = User(email=email, password=hashed_password, name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template("register.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('secrets'))
+        else:
+            flash('Login failed. Check your email and/or password.', 'danger')
+            return redirect(url_for('login'))
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    return render_template("secrets.html", name=current_user.name)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static', 'files/cheat_sheet.pdf')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
