2df7e28c0e4cc10bc4f78c138246502f1ce773a2
Robert <rquesadaqq@outlook.com>
2025-08-27 21:56:00 -0600

    Advance with day 74 using data

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bob/100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/venv
+set -gx VIRTUAL_ENV /home/bob/Github/100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
